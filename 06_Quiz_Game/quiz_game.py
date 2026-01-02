02b24ecc16c81671e392b320db33d99d216a84b6
SatriaDwiCahyaa <zatriagithub@gmail.com>
2026-01-02 21:21:48 +0700

    Perencaan awal Quiz Game.
    Merencakan fungsi apa saja yang akan digunakan untuk mempermudah dalam belajar tanpa harus melihat kode yang sudah jadi.

---
+++ 06_Quiz_Game/quiz_game.py
@@ -0,0 +1,40 @@
+"""
+Quiz Game
+Author: Satria Dwi Cahya
+Purpose: 
+"""
+# Module Import Section
+import os
+import random
+
+# digunakan untuk keyword dicitonary nantinya
+QUESTION = 'question'
+OPTIONS = 'options'
+ANSWER = 'answer'
+
+# Untuk membersihkan teks pada terminal
+def clear_screen():
+    os.system('cls')
+
+# Menyambut pengguna agar terasa lebih dekat
+def welcome_sign():
+    print('Quiz Game')
+    print('---------')
+
+
+# -----
+# Gamee
+# -----
+
+
+# Memberikan pertanyaan ke user
+def ask_question():
+    print()    
+
+# Mengacak pertanyaan, menentukan jawaban benar, dan menentukan skor
+def run_quiz():
+    print()
+
+# Menyimpan pertanyaan dengan list dan dictionary di dalam list
+def storage_question():
+    print()
