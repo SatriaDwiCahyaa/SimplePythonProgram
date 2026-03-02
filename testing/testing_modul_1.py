4bcebe3896ba6cb7ef179c40404bd45ead3edcd6
SatriaDwiCahyaa <zatriagithub@gmail.com>
2026-03-02 14:13:49 +0700

    test: deklarasi variabel dasar.

---
+++ testing/testing_modul_1.py
@@ -0,0 +1,10 @@
+string_teks            = "Praktikum AP II, Kelompok 3"
+bilangan_bulat_integer = 10
+desimal_rill           = 69.96
+kondisi_boolean        = True
+
+
+print(type(string_teks))
+print(type(bilangan_bulat_integer))
+print(type(desimal_rill))
+print(type(kondisi_boolean))
