250202cf3a88b8aad28bc656ca8a41a697ae538e
SatriaDwiCahyaa <zatriagithub@gmail.com>
2026-03-07 11:33:58 +0700

    feat: menambah studi kasus pada program perbaikan validasi input dengan class

---
+++ praktikum/algorithms_programming_II/modul_2/2nd_perbaikan_program.py
@@ -0,0 +1,49 @@
+def welcome_sign():
+    print("=" * 50)
+    print("  TUGAS 2 - CLASS MyInput")
+    print("=" * 50)
+
+
+def sebelum_perbaikan():
+    class MyInput:
+        def baca_string(self):
+            return input()
+        def baca_int(self):
+            while True:
+                try:
+                    return int(self.baca_string())
+                except ValueError:
+                    print("Input harus berupa angka! Silakan coba lagi.")
+
+
+def tugas2_validasi_input():
+    class MyInput:
+        """Class untuk membaca input dengan validasi tipe data."""
+
+        def baca_string(self):
+            """Membaca input sebagai string."""
+            return input()
+
+        def baca_int(self):
+            """Membaca input sebagai integer dengan validasi loop."""
+            while True:
+                try:
+                    return int(self.baca_string())
+                except ValueError:
+                    print("Input harus berupa angka! Silakan coba lagi.")
+
+    # Contoh penggunaan
+    mi = MyInput()
+    print("  Masukkan nama Anda:")
+    nama = mi.baca_string()
+    print("  Masukkan usia Anda:")
+    usia = mi.baca_int()
+    print(f"\n  Halo, {nama}! Usia Anda adalah {usia} tahun.\n")
+
+
+def main():
+    tugas2_validasi_input()
+
+
+if __name__ == "__main__": 
+    main()
